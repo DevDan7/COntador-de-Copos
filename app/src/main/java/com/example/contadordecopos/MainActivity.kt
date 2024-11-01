d4a2470e663adfa8f5ef8fd15f272176c1f6b820
Vdaniel <danielsvillegas17@gmail.com>
2024-11-01 10:17:37 -0300

    Primer commit

---
+++ app/src/main/java/com/example/contadordecopos/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.contadordecopos
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    val contador: TextView by lazy { findViewById(R.id.contador) }
+    val btnMaisUm: Button by lazy { findViewById(R.id.maisCopos) }
+    val btnMenosUm: Button by lazy { findViewById(R.id.menosCopos) }
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
